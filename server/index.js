d874c5055e7f9aa6d1d19045497d3ea27e14d2a5
rjsiinghhh <>
2020-12-15 21:19:07 -0800

    13

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+
 const PORT = process.env.PORT || 5000
 
 const router = require('./router');
@@ -12,6 +13,13 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
   console.log('We have a new connection');
+
+socket.on('join', ({ name, room }, callback) => {
+console.log(name, room);
+
+
+
+});
 
   socket.on('disconnect', () => {
     console.log('User has left');
@@ -24,5 +32,4 @@ server.listen(PORT, () => console.log(`Server has started on PORT ${ PORT }`));
 
 
 
-
-// 36
+// 51
